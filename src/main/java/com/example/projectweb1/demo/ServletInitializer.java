afaff7248ea941e8a242733facf89a8245d36db8
MIGUEL FLOR <mickybasic@gmail.com>
2020-09-30 16:08:34 +0200

    Initial commit

---
+++ src/main/java/com/example/projectweb1/demo/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.example.projectweb1.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Projectweb1Application.class);
+    }
+}
